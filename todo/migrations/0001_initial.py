c7735a9cadad60ada98629d492b2ceaa362b6e5b
1F10240087 <s1f102400875@iniad.org>
2025-07-09 23:19:44 +0900

    コードを整形

--- todo/migrations/0001_initial.py
+++ todo/migrations/0001_initial.py
@@ -8,18 +8,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='task',
+            name="task",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('comleted', models.BooleanField(default=False)),
-                ('posted_at', models.DateTimeField(default=django.utils.timezone.now)),
-                ('due_at', models.DateTimeField(blank=True, null=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                ("comleted", models.BooleanField(default=False)),
+                ("posted_at", models.DateTimeField(default=django.utils.timezone.now)),
+                ("due_at", models.DateTimeField(blank=True, null=True)),
             ],
         ),
     ]

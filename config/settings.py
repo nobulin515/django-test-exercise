2950dbb02a14dcb530c9a699b0c096e69dec7c4a
s1f102400875 <s1f102400875@iniad.org>
2026-06-18 13:43:31 +0900

    Taskモデルを追加

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
     
 ]
 
@@ -48,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'todo',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
